e14f5bb31735b47de4fe34cad6d3f139403bee4c
disasterrific <halloumi.stone@gmail.com>
2019-03-15 00:43:38 +0100

    Putting functions for the game together

--- TicTacToe.py
+++ TicTacToe.py
@@ -1,4 +1,3 @@
-
 #%%
 #Step 1: Function to display the board
 
@@ -17,81 +16,74 @@ def display_board(board):
     print('---------')
     print(board[1]+' |',board[2]+' | '+board[3])
 
-
-#%%
 # test if board displaying works
 
 test_board = ['#','1','2','3','4','5','6','7','8','9']
 
 display_board(test_board)
 
-
 #%%
 # Step 2: Function to ask players for marker (X or O)
 
 def player_input():
-    
+    '''
+    OUTPUT = {'Player 1': Player 1 marker, 'Player 2': Player 2 marker}
+    '''
     print('Hello, this is Tic Tac Toe.')
     
     marker = ''
     
     while marker != 'X' and marker != 'O':
-        marker = input('Player 1, choose X or O: ')
+        marker = input('Player 1, choose X or O: ').upper()
      
     if marker == 'X':
-        return ('X','O')
+        return markers = {'Player 1': 'X','Player 2': 'O'}
     
     else:
-        return ('O','X')
+        return markers = {'Player 1': 'O', 'Player 2': 'X'}
         
-    return (player1, player2)
-
-    #player1_marker, player2_marker = (player1, player2)
-
-
-#%%
+    
+#%%  
 #test
 player_input()
-
+return tuple(markers.values())
 
 #%%
-# Step 3: Function to position markers on board
+# Step 3: Function to position marker on board
 
 def place_marker(board, marker, position):
+    int(input(print(f'{choose_first()} begins! Press the corresponding number on your num pad to set your marker on the Tic Tac Toe Board:')))
     board[position] = marker
-
 
 #%%
 #test
 place_marker(test_board, 'T', 4)
 display_board(test_board)
 
-
 #%%
-# Step 4: Function to take in marker input and test is a play has won
-def win_check(board,mark):
+# Step 4: Function to take in marker input and test if a player has won
+def win_check(board,marker):
     
-    if ((board[1] == mark and board[2] == mark and board[3] == mark) or 
-    (board[4] == mark and board[5] == mark and board[6] == mark) or
-    (board[7] == mark and board[8] == mark and board[9] == mark) or
-    (board[1] == mark and board[4] == mark and board[7] == mark) or
-    (board[2] == mark and board[5] == mark and board[8] == mark) or
-    (board[3] == mark and board[6] == mark and board[9] == mark) or
-    (board[1] == mark and board[5] == mark and board[9] == mark) or
-    (board[7] == mark and board[5] == mark and board[3] == mark)):
+    if ((board[1] == marker and board[2] == marker and board[3] == marker) or 
+    (board[4] == marker and board[5] == marker and board[6] == marker) or
+    (board[7] == marker and board[8] == marker and board[9] == marker) or
+    (board[1] == marker and board[4] == marker and board[7] == marker) or
+    (board[2] == marker and board[5] == marker and board[8] == marker) or
+    (board[3] == marker and board[6] == marker and board[9] == marker) or
+    (board[1] == marker and board[5] == marker and board[9] == marker) or
+    (board[7] == marker and board[5] == marker and board[3] == marker)):
         
         #alternative:
         #str = ''
-        #str.join(board[4,5,6]) == player1_marker*3
-        #str.join(board[7,8,9]) == player1_marker*3
+        #str.join(board[4,5,6]) == marker*3
+        #str.join(board[7,8,9]) == marker*3
         
-        print(f'Congratulations! {mark} wins!')
-        pass
-
+        print(f'Congratulations! {markers.key('marker')} wins!')
+        break
 
 #%%
+#test
 win_check(test_board,'T')
-
 
 #%%
 # Step 5: Function to pick player that starts
@@ -104,14 +96,12 @@ def choose_first():
     else:
         return 'Player 2'
 
-
 #%%
 # Step 6: Check if there is an open space on the board
 
 def space_check(board, position):
     
     return board[position] == ' '
-
 
 #%%
 # Step 7: Check if board is full
@@ -123,8 +113,6 @@ def full_board_check(board):
             return False
     return True
         
-
-
 #%%
 # Step 8: Ask for position input and check if position is free
 
@@ -136,12 +124,10 @@ def player_choice(board):
         position = int(input('Choose your next position (1-9): ')) 
         
     return position
-    
 
-
-#%%
+#%%    
+#test
 player_choice(test_board)
-
 
 #%%
 # Step 9: Ask for replay
@@ -150,9 +136,52 @@ def replay():
     
     return input('Do you want to play again? [yes/no]').lower().startswith('y')
 
-
 #%%
 # Step 10: Put functions together to run the game 
+
+def play_TicTacToe()
+    
+    empty_board = ['#','','','','','','','','','']
+    # Step 7
+    while game_ongoing == True:
+         # Step1   
+         display_board(empty_board)
+         # Step 2
+         player_input()
+         # Step 5
+         print(f'{choose_first()} begins! Choose your position: ')
+         # Step 3   
+         place_marker(empty_board, marker, position)
+         # Steps 8 and 6
+         player_choice(empty_board)         
+         # Step 4
+         if win_check(empty_board,marker):
+
+    else:
+        # Step 7
+        if full_board_check(board):
+             print("It's a tie!") 
+
+    # Step 9
+    if replay() == 'y':
+        clear_board
+        
+
+
+
+
+
+
+
+
+
+         
+
+
+
+
+
+
 
 
 #%%
